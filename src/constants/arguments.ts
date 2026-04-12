ed68f705fe5c6a0fc57ab1d02d0b86ba6c7dccd3
Soria Garcia, Jose Eduardo <jesoria@indra.es>
2026-04-12 10:22:56 +0200

    feat: inicio comando run

---
+++ src/constants/arguments.ts
@@ -0,0 +1,11 @@
+
+export const ARGV_NAME_CONFIGURATION = 'configuration';
+export const ARGV_SHORT_NAME_CONFIGURATION = 'c';
+
+export const ARGV_NAME_DEBUG = 'debug';
+export const ARGV_SHORT_NAME_DEBUG = 'd';
+
+export const ARGV_NAME_WATCH = 'watch';
+
+export const ARGV_NAME_ENV_FILE = 'env-file';
+export const ARGV_NAME_NO_BANNER = 'no-banner';
---
+++ src/constants/environment.ts
@@ -0,0 +1,9 @@
+/** Environment */
+export const NODE_ENV = 'NODE_ENV';
+export const ENV_DEBUG_MODE = 'DEBUG_MODE';
+export const ENV_WATCH_MODE = 'WATCH_MODE';
+
+/** Values */
+export const DEVELOPMENT = 'development';
+export const PRODUCTION = 'production';
+export const TEST = 'test';
